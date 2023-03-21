01059cb2274b911649b75dc25d0805c4ca1ad2d5
Julian Maurice <julian.maurice@biblibre.com>
2023-03-21 10:02:10 +0100

    Show linked taxonomies and terms in "Linked resources" tab

--- src/Api/Adapter/TaxonomyTermAdapter.php
+++ src/Api/Adapter/TaxonomyTermAdapter.php
@@ -5,6 +5,7 @@ use Doctrine\ORM\QueryBuilder;
 use Omeka\Api\Adapter\AbstractResourceEntityAdapter;
 use Omeka\Api\Request;
 use Omeka\Entity\EntityInterface;
+use Omeka\Entity\Resource;
 use Omeka\Stdlib\ErrorStore;
 use Taxonomy\Api\Representation\TaxonomyTermRepresentation;
 use Taxonomy\Entity\TaxonomyTerm;
@@ -84,5 +85,44 @@ class TaxonomyTermAdapter extends AbstractResourceEntityAdapter
             $this->authorize($taxonomy, 'add-term');
             $entity->setTaxonomy($taxonomy);
         }
+    }
+
+    /**
+     * Get the query builder needed to get subject values.
+     *
+     * Note that the returned query builder does not include $qb->select().
+     *
+     * @param Resource $resource
+     * @param int|null $property
+     * @return QueryBuilder
+     */
+    public function getSubjectValuesQueryBuilder(Resource $resource, $property = null)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->from('Omeka\Entity\Value', 'v')
+            ->join('v.resource', 'r')
+            ->where($qb->expr()->eq('v.valueResource', $this->createNamedParameter($qb, $resource)))
+            // Limit subject values to those belonging to primary resources.
+            ->andWhere($qb->expr()->orX(
+                'r INSTANCE OF Omeka\Entity\Item',
+                'r INSTANCE OF Omeka\Entity\ItemSet',
+                'r INSTANCE OF Omeka\Entity\Media',
+                'r INSTANCE OF Taxonomy\Entity\Taxonomy',
+                'r INSTANCE OF Taxonomy\Entity\TaxonomyTerm'
+            ));
+        if ($property) {
+            $qb->andWhere($qb->expr()->eq('v.property', $this->createNamedParameter($qb, $property)));
+        }
+        // Need to check visibility manually here
+        $services = $this->getServiceLocator();
+        $acl = $services->get('Omeka\Acl');
+        $identity = $services->get('Omeka\AuthenticationService')->getIdentity();
+        if (!$acl->userIsAllowed('Omeka\Entity\Resource', 'view-all')) {
+            $qb->andWhere($qb->expr()->orX(
+                $qb->expr()->eq('r.isPublic', '1'),
+                $qb->expr()->eq('r.owner', $this->createNamedParameter($qb, $identity))
+            ));
+        }
+        return $qb;
     }
 }
